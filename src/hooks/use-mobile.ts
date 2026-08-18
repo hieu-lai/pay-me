2363ebc61892fa517bde8dcbd76f2f11573d544f
hieulai <hieuqlai@gmail.com>
2026-08-18 10:21:07 +1000

    💄 style: format repository sources (#46)

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,13 +1,13 @@
-import * as React from "react"
+import * as React from 'react'
 
 const MOBILE_BREAKPOINT = 768
 const MOBILE_MEDIA_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
 
 function subscribe(callback: () => void) {
   const mediaQuery = window.matchMedia(MOBILE_MEDIA_QUERY)
-  mediaQuery.addEventListener("change", callback)
+  mediaQuery.addEventListener('change', callback)
 
-  return () => mediaQuery.removeEventListener("change", callback)
+  return () => mediaQuery.removeEventListener('change', callback)
 }
 
 function getSnapshot() {
